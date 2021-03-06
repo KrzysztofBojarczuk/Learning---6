01ff4be7a8835cc54ed061d5a87417f0ef024ef6
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-03-06 10:31:42 +0100

    Add project files.

---
+++ Konwersje typów i rzutowania1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Konwersje_typów_i_rzutowania1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            int yearOfBirth;
+            if (int.TryParse(userInput, out yearOfBirth))
+            {
+                int age = DateTime.Now.Year - yearOfBirth;
+                Console.WriteLine("You are " + age);
+            }
+            else
+            {
+                Console.WriteLine("Incoret value");
+            }
+
+        }
+    }
+}
